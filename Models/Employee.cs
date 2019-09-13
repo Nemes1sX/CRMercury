cbd3ad9c38e20a62c3ec99ca1220f1e0ca875610
Vytautas <Vytautas@PC>
2019-09-13 19:21:17 +0200

    First commit from vs. Employee table

---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Employee
+{
+    public int id { get; set; }
+    public string lastName { get; set; }
+    public string firstName { get; set; }
+    public int companyId { get; set; }
+
+    public IList<Task> tasks { get; set; }
+}
